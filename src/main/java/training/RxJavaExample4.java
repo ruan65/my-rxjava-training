35f0b06592e2e885cb8f5d57c84fefc5f5ff8308
a <ruan65@gmail.com>
2016-10-29 00:51:34 +0300

    some playing around....

--- src/main/java/training/RxJavaExample4.java
+++ src/main/java/training/RxJavaExample4.java
@@ -2,7 +2,6 @@ package training;
 
 import rx.Observable;
 import rx.Observer;
-import rx.functions.Action1;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,13 +11,13 @@ import java.util.List;
  */
 public class RxJavaExample4 {
 
-    public static Observable<String> getFirstLetter(String s) {
-        return Observable.just(s.substring(0, 1));
+    public static Observable<String> getFirst2Letters(String s) {
+        return Observable.just(s.substring(0, 2));
     }
 
     public static void main(String[] args) {
 
-        Observable<List<String>> just = Observable.just(Arrays.asList("one", "two", "three", "aha!!!!", "zahhhhhhqaa"));
+        Observable<List<String>> just = Observable.just(Arrays.asList("one", "two", "three", "ok", "aha!!!!", "zahhhhhhqaa"));
 
 
 //        just.subscribe(it -> System.out.println(it));
@@ -33,7 +32,13 @@ public class RxJavaExample4 {
 
         just
                 .flatMap(it -> Observable.from(it))
-                .flatMap(it -> getFirstLetter(it))
-                .subscribe(it -> System.out.println(it));
+                .flatMap(it -> getFirst2Letters(it))
+                .map(it -> it.toUpperCase())
+                .map(it -> it + "!!!")
+//                .subscribe(it -> System.out.println(it));
+
+        .subscribe(s -> System.out.println(s),
+                er -> System.out.println(er.getCause()),
+                () -> System.out.println("We have completed!!!"));
     }
 }
