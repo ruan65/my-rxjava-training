5cf7db1fbcb37e325818aa788b2e0bed49e63a6b
a <ruan65@gmail.com>
2016-10-29 16:18:00 +0300

    with composite subscription

--- src/main/java/training/RxJavaExample5_unsubscribe.java
+++ src/main/java/training/RxJavaExample5_unsubscribe.java
@@ -3,6 +3,7 @@ package training;
 import rx.Observable;
 import rx.Subscription;
 import rx.subjects.PublishSubject;
+import rx.subscriptions.CompositeSubscription;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,22 +20,36 @@ public class RxJavaExample5_unsubscribe {
 
         ps.onNext(100);
 
+        CompositeSubscription comp = new CompositeSubscription();
+
         Subscription subscription1 = ps.subscribe(i -> System.out.println("sbs_1: " + i));
         Subscription subscription2 = ps.subscribe(i -> System.out.println("sbs_2: " + i));
 
+        comp.add(subscription1);
+        comp.add(subscription2);
         ps.onNext(200);
         ps.onNext(777);
 
-        subscription1.unsubscribe();
+//        subscription1.unsubscribe();
+
+        comp.remove(subscription1);
+
         System.out.println("sbs_1 has been unsubscribed");
 
         ps.onNext(300);
         ps.onNext(0);
 
-        ps.subscribe(i -> System.out.println("sbs_3: " + i));
+        comp.add(ps.subscribe(i -> System.out.println("sbs_3: " + i)));
 
         ps.onNext(1);
 
+        ps.onNext(1000000);
+
+        System.out.println("removing......");
+
+        comp.clear();
+
+        ps.onNext(999);
 
 
 
